ec20f7fe3b4674f715b50751970b6e8e2d511f7a
Tianomiano <patomiano6@gmail.com>
2022-11-23 00:52:54 -0800

    spec

---
+++ printf.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+*specifiers - checks for specifiers
+*@format: format spec
+*Return: pointer to func
+*/
+static int(*specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+		print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*p[i].t == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+/**
+*_printf -prints
+*@format: list of arg tpes
+*Return: no of char printed
+*/
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, j = 0;
+	va_list op;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(op, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			j++;
+		}
+		if (!format[i])
+			return (j);
+		f = specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			j += f(op);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		j++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(op);
+	return (j);
+}
